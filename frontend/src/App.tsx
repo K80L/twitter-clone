7382250919b70536d6f735e604ca8c25e8116af3
Kevin Ly <kevintly1@Kevins-MacBook-Air.local>
2022-05-08 21:41:33 -0700

    Redo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header";
+
+function App() {
+  console.log("test");
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import "./Header.css";
+
+export const Header = () => {
+  return <h1 className="header">Twitter</h1>;
+};
